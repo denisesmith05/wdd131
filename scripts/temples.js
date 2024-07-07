6003f63d146c2c6c8b6e09ac1b80404c5466565c
Denise <denise.sunshine@icloud.com>
2024-07-06 23:51:19 -0700

    updated finally temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,17 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const lastModifiedSpan = document.getElementById("lastModified");
     const menuToggle = document.getElementById("menuToggle");
     const menu = document.getElementById("menu");
-    
+
     yearSpan.textContent = new Date().getFullYear();
-    lastModifiedSpan.textContent = `Last Modified: ${document.lastModified}';
-    
+    lastModifiedSpan.textContent = `Last Modified: ${document.lastModified}`;
+
     menuToggle.addEventListener("click", function() {
         if (menu.style.display === "flex") {
-        menu.style.display = "none";
-        menuToggle.textContent = "☰";
+            menu.style.display = "none";
+            menuToggle.textContent = "☰";
         } else {
-        menu.style.display = "flex";
-        menuToggle.textContent = "X";
+            menu.style.display = "flex";
+            menuToggle.textContent = "X";
         }
-    })
+    });
 });
